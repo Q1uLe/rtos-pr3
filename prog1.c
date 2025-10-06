27c4264232f98aea6b3fae03a50f94d35c4d125c
Q1uLe <sharipov.r.v@edu.mirea.ru>
2025-10-07 00:10:37 +0300

    Added file headers and function's descriptions

--- prog1.c
+++ prog1.c
@@ -1,12 +1,43 @@
+/*--------------------------------------------------------*/
+/* ИМЯ: SHARIPOV RENAT, ID ПОЛЬЗОВАТЕЛЯ: 23К0210          */
+/* СРОК: 06/10/2025                                       */
+/* ИМЯ ФАЙЛА: prog1.c                                     */
+/* НАЗНАЧЕНИЕ ПРОГРАММЫ:                                  */
+/* Программа демонстрирует работу с процессами, созданиие */
+/* дочерних процессов, обработку сигналов                 */
+/*--------------------------------------------------------*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
 
+
+/*--------------------------------------------------------*/
+/* ФУНКЦИЯ: signal_handler                                */
+/* Данная функция получает и обрабатывается сигнал        */
+/* ИСПОЛЬЗУЕМЫЕ ПАРАМЕТРЫ:                                */
+/* int sig - номер полученного сигнала                    */
+/* ВЫЗЫВАЕМАЯ ФУНКЦИЯ:                                    */
+/* printf()                                               */
+/*--------------------------------------------------------*/
+
+
 void signal_handler(int sig){
     printf("Получен сигнал %d\n", sig);
 }
+
+/*--------------------------------------------------------*/
+/* ФУНКЦИЯ: main                                          */
+/* В данной функции заключена основная логика             */
+/* программы                                              */
+/* ИСПОЛЬЗУЕМЫЕ ПАРАМЕТРЫ: отсутствуют                    */
+/* ВЫЗЫВАЕМЫЕ ФУНКЦИИ:                                    */
+/* printf(), signal(), scanf(), fork(), perror(), execl() */
+/* getpid(), getppid(), waitpid(), WIFEXITED(),           */
+/* WEXITSTATUS(), WIFSIGNALED(), WTERMSIG(), exit()       */
+/*--------------------------------------------------------*/
+
 
 int main() {
     int status;
@@ -17,7 +48,7 @@ int main() {
     signal(SIGTERM, signal_handler);
 
     printf("Input your name: ");
-    if (scanf("%99s", user_input) != 1){
+    if (scanf("%99s", user_input) != 1) {
         printf("Input error!\n");
         return 1;
     }
@@ -26,21 +57,21 @@ int main() {
 
     pid = fork();
 
-    if(pid == -1){
-        perror("Error while executing fork()\n");
+    if (pid == -1) {
+        perror("Error while executing fork()");
         return 1;
     }
-    else if(pid == 0){
-        printf("Child proccess: PID = %d, PPID = %d", getpid(), getppid());
+    else if (pid == 0) {
+        printf("Child proccess: PID = %d, PPID = %d\n", getpid(), getppid());
         execl("./process", "process", user_input, NULL);
-        perror("Error while executing execl()\n");
+        perror("Error while executing execl()");
         exit(1);
     } 
     else {
         printf("Parent process: PID = %d, created child process with PID = %d\n", getpid(), pid);
         printf("Wait child process close...\n");
         waitpid(pid, &status, 0);
-        if (WIFEXITED(status)){
+        if (WIFEXITED(status)) {
             printf("Child process ended with exit code: %d\n", WEXITSTATUS(status));
         }
         else if (WIFSIGNALED(status)) {
@@ -50,6 +81,4 @@ int main() {
     printf("Programm ended up. Goodbye, %s!\n", user_input);
     return 0;
 }
-
-
 
